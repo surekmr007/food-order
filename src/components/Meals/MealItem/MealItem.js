801b44fa54c22279ad00e6364f583d01d8afecc3
surekmr007 <surekmr007@gmail.com>
2023-02-25 14:46:57 +0530

    meals layout change

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,21 +1,23 @@
-import { useContext } from 'react';
+import { useContext } from "react";
+import { toast } from "react-hot-toast";
 
-import MealItemForm from './MealItemForm';
-import classes from './MealItem.module.css';
-import CartContext from '../../../store/cart-context';
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const price = `$${props.price.toFixed(2)}`;
+  const price = `${props.price.toFixed(2)}`;
 
-  const addToCartHandler = amount => {
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
-      price: props.price
+      price: props.price,
     });
+    toast.success(`${amount} ${props.name} was added `);
   };
 
   return (
@@ -23,7 +25,7 @@ const MealItem = (props) => {
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
+        <div className={classes.price}>₹{price}</div>
       </div>
       <div>
         <MealItemForm onAddToCart={addToCartHandler} />
